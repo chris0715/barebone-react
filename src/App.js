b5ab541cecd7ea2d97309f8c693dbfba0b865608
cristopher Encarnacion <cencarnacion@Cristopher.local>
2017-10-23 08:30:29 -0400

    added the last part needede for the boilerplate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React from 'react'
+import React, {Component} from 'react'
 //import cat from './assets/images/jesus.jpeg'
 
 
 class App extends React.Component {
   render() {
     return(
-      <div className="container">
-        <h1 className="container"> This is sparta</h1>
-      </div>
+      <div>dd</div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
 export default App
